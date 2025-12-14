62d306581ef78d9c4334666955c002c98521c280
AshenEditZ <officialcyber97@outlook.com>
2025-12-14 08:00:08 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    botName: "Queen Selina💞",
+    ownerName: "AshenEditZ",
+    ownerNumber: "94768738555", // Format: CountryCodeNumber (No +)
+    
+    // Admin Login
+    adminEmail: "ashen.editz@gmail.com",
+    adminPass: "ashen@123",
+    
+    // API Keys
+    infinityApiKey: "Infinity-manoj-x-mizta",
+    
+    // Links & Media
+    botImage: "https://i.imgur.com/rm1qWjR.jpeg",
+    channelJid: "120363160000000000@newsletter", // Example Newsletter JID
+    channelLink: "https://whatsapp.com/channel/0029VavLxme5PO0yDv3eUa47",
+    
+    // System Limits
+    maxBots: 50, // Limit for Replit RAM safety
+    prefix: "."
+};
